049859c704075c461b82e0a23c73362da4a0df93
Kafi <ahkafi05@gmail.com>
2025-03-11 05:15:18 +0600

    add liked pet section

--- script.js
+++ script.js
@@ -8,14 +8,13 @@ const getDataByCategoryName = async (name) =>{
 }
 
 
-const getPetsData = async () =>{
+const getData = async () =>{
     const URL = `${BASE_URL}/pets`;
     const response = await fetch(URL);
     const fetchedPetsData = await response.json();
-    console.log(fetchedPetsData)
     petCard(fetchedPetsData.pets)
 }
-getPetsData()
+getData()
 
 const clickedBtn = (idName)=>{
     const category = document.querySelectorAll(".category");
@@ -36,12 +35,10 @@ const clickedBtn = (idName)=>{
 }
 
 const petCard =(data)=>{
-    console.log(data);
     let count = 0;
     
     document.getElementById("card-box").innerHTML = "";
     if(data.length === 0) noDataFound();
-    console.log(data.length)
     for(let i = 0; i<data.length; i++){
         if(count === 3){
             break;
@@ -61,7 +58,7 @@ const petCard =(data)=>{
                      </div>
                  </div>
                  <div class="h-2/10 w-full rounded-md flex justify-between items-end">
-                     <button class="py-2 px-4 rounded-md font-bold text-sm text-[#0e7a81] border-2 border-[#1313131a] "><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                     <button id = "like-btn-${count}" onclick = "addClickEvent(${count})" class="py-2 px-4 rounded-md font-bold text-sm text-[#0e7a81] border-2 border-[#1313131a] "><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                          <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
                      </svg>
                      </button>
@@ -111,3 +108,23 @@ document.getElementById("rabbit").addEventListener("click", async ()=>{
     const data = await getDataByCategoryName("rabbit");
     petCard(data);
 })
+
+let likedCount = 0;
+const addClickEvent = (idNum)=>{
+        if(likedCount < 6){
+            const likedBtn = document.getElementById(`like-btn-${idNum}`);
+
+            const likedBox = document.getElementById("liked-box");
+
+            const imageLink = likedBtn.parentElement.parentElement.querySelector('img').src;
+            console.log(imageLink)
+            const imageDiv = document.createElement("div")
+            imageDiv.classList.add("rounded-lg", "h-full", "w-full")
+            imageDiv.innerHTML = 
+            `<img class = "h-full w-full rounded-lg" src = ${imageLink} alt = "pet image">
+            `
+            console.log(imageDiv);
+            likedBox.append(imageDiv);
+            likedCount++;
+        }
+}
